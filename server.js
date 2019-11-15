a3cfe738c57f4ad3e6302f83f0eaca41b5f58a2a
Joseph Tatum <josephtatum@gmail.com>
2019-11-15 09:51:37 -0800

    setting up heroku

--- server.js
+++ server.js
@@ -6,32 +6,27 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const client = require('./lib/client');
+
 // Initiate database connection
 client.connect();
 
 // Auth
-
 const ensureAuth = require('./lib/auth/ensure-auth');
 const createAuthRoutes = require('./lib/auth/create-auth-routes');
-
 const authRoutes = createAuthRoutes({
     selectUser(email) {
         return client.query(`
-            SELECT id, email, hash, display_name
-            FROM users
-            WHERE email = $1;
+
         `,
-            [email]
+            [eail]
         ).then(result => result.rows[0]);
     },
     insertUser(user, hash) {
 
         return client.query(`
-            INSERT into users (email, hash, display_name)
-            VALUES ($1, $2, $3)
-            RETURNING id, email, display_name;
+
         `,
-            [user.email, hash, user.display_name]
+            []
         ).then(result => result.rows[0]);
     }
 });
@@ -49,99 +44,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api', ensureAuth);
 
 // API Routes
-
-// *** TODOS ***
-
-app.get('/api/test', (req, res) => {
-    res.json({
-        message: `the user's id is ${req.userId}`
-    });
-});
-
-
-app.get('/api/todos', async (req, res) => {
-
-    try {
-        const todos = await client.query(`
-           SELECT *
-           FROM todos
-           WHERE user_id=$1
-       `, [req.userId]);
-
-        res.json(todos.rows);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: err.message || err
-        });
-    }
-
-});
-
-app.post('/api/todos', async (req, res) => {
-    const todo = req.body;
-    try {
-        const result = await client.query(`
-            INSERT INTO todos (task, complete, user_id)
-            VALUES ($1, $2, $3)
-
-            RETURNING *;
-        `,
-            [todo.task, todo.complete, req.userId]);
-
-        res.json(result.rows[0]);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: err.message || err
-        });
-    }
-});
-
-app.put('/api/todos/:id', async (req, res) => {
-    const id = req.params.id;
-    const todo = req.body;
-
-    try {
-        const result = await client.query(`
-            UPDATE todos
-            SET    complete = $2
-            WHERE  id = $1
-            RETURNING *;
-        `, [id, todo.complete]);
-
-        res.json(result.rows[0]);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: err.message || err
-        });
-    }
-});
-
-app.delete('/api/todos/:id', async (req, res) => {
-    // get the id that was passed in the route:
-    const id = req.params.id; // ???
-
-    try {
-        const result = await client.query(`
-            DELETE FROM todos
-            WHERE id = $1
-            RETURNING *;
-        `, [id]);
-
-        res.json(result.rows[0]);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: err.message || err
-        });
-    }
-});
 
 // Start the server
 app.listen(PORT, () => {
