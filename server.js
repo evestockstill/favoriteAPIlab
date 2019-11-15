30a176b0e381f3abc6e75b8f5233bfe2b6af594b
Joseph Tatum <josephtatum@gmail.com>
2019-11-15 09:35:31 -0800

    continuing to create a foundation

--- server.js
+++ server.js
@@ -6,44 +6,144 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const client = require('./lib/client');
-
+// Initiate database connection
 client.connect();
 
 // Auth
+
 const ensureAuth = require('./lib/auth/ensure-auth');
 const createAuthRoutes = require('./lib/auth/create-auth-routes');
+
 const authRoutes = createAuthRoutes({
     selectUser(email) {
         return client.query(`
-            SELECT id, email, hash, display_name as "displayName" 
+            SELECT id, email, hash, display_name
             FROM users
             WHERE email = $1;
         `,
-        [email]
+            [email]
         ).then(result => result.rows[0]);
     },
     insertUser(user, hash) {
-        console.log(user);
+
         return client.query(`
             INSERT into users (email, hash, display_name)
             VALUES ($1, $2, $3)
-            RETURNING id, email, display_name as "displayName";
+            RETURNING id, email, display_name;
         `,
-        [user.email, hash, user.displayName]
+            [user.email, hash, user.display_name]
         ).then(result => result.rows[0]);
     }
 });
 
+
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
-app.use(morgan('dev')); 
-app.use(cors()); 
-app.use(express.static('public')); 
-app.use(express.json()); 
-
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // server files from /public folder
+app.use(express.json()); // enable reading incoming json data
 
 app.use('/api/auth', authRoutes);
-
-// everything that starts with "/api" below here requires an auth token!
 app.use('/api', ensureAuth);
+
+// API Routes
+
+// *** TODOS ***
+
+app.get('/api/test', (req, res) => {
+    res.json({
+        message: `the user's id is ${req.userId}`
+    });
+});
+
+
+app.get('/api/todos', async (req, res) => {
+
+    try {
+        const todos = await client.query(`
+           SELECT *
+           FROM todos
+           WHERE user_id=$1
+       `, [req.userId]);
+
+        res.json(todos.rows);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+
+});
+
+app.post('/api/todos', async (req, res) => {
+    const todo = req.body;
+    try {
+        const result = await client.query(`
+            INSERT INTO todos (task, complete, user_id)
+            VALUES ($1, $2, $3)
+
+            RETURNING *;
+        `,
+            [todo.task, todo.complete, req.userId]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.put('/api/todos/:id', async (req, res) => {
+    const id = req.params.id;
+    const todo = req.body;
+
+    try {
+        const result = await client.query(`
+            UPDATE todos
+            SET    complete = $2
+            WHERE  id = $1
+            RETURNING *;
+        `, [id, todo.complete]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+    // get the id that was passed in the route:
+    const id = req.params.id; // ???
+
+    try {
+        const result = await client.query(`
+            DELETE FROM todos
+            WHERE id = $1
+            RETURNING *;
+        `, [id]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
